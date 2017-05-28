16d28e3833e02d1aed79b6fcfc053b23a3b6fe2f
David Attarzadeh <dattar01@gmail.com>
2017-05-28 13:26:38 -0700

    Fix favorite_sports join table migration and add association to Sport model.

--- db/migrate/20170528010255_favorite_sports.rb
+++ db/migrate/20170528010255_favorite_sports.rb
@@ -1,6 +1,6 @@
 class FavoriteSports < ActiveRecord::Migration[5.1]
   def change
-	create_join_table :users, :sports, table_name: :favorite_sports, do |t|
+	create_join_table :users, :sports, table_name: :favorite_sports do |t|
 	  t.index :user_id
 	  t.index :sport_id
 	end

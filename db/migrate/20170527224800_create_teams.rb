1003a51bd6a342cdd209f60f847c5e43e54cea42
David Attarzadeh <dattar01@gmail.com>
2017-05-27 17:14:02 -0700

    Add scaffolds for orgs, teams, and users. Update team and user migration files.

---
+++ db/migrate/20170527224800_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teams do |t|
+      t.string :name, :null => false
+      t.text :description
+
+      t.integer :organization_id, :null => false
+
+      t.timestamps
+    end
+  end
+end
